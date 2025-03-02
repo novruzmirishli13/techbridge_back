779544c5682d3ec15155203174d33d46076cca33
novruzmirishli13 <nmirisli13@gmail.com>
2025-03-02 18:05:10 +0400

    sfeafqawndfoD45

--- src/main/java/com/example/HZT/HztApplication.java
+++ src/main/java/com/example/HZT/HztApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HztApplication {
 
 	public static void main(String[] args) {
-		int port = Integer.parseInt(System.getenv("PORT"));
+		Integer.parseInt(System.getenv("PORT"));
 		SpringApplication.run(HztApplication.class, args);
 		
 	}
