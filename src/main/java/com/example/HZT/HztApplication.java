b54be748dbddc962409891965b1b790fb0f5432b
iltifatibad <iltifatibad@gmail.com>
2025-03-06 21:42:21 +0000

    A

--- src/main/java/com/example/HZT/HztApplication.java
+++ src/main/java/com/example/HZT/HztApplication.java
@@ -7,18 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HztApplication {
 
     public static void main(String[] args) {
-        String portEnv = System.getenv("PORT");
+        // Portu doğrudan 8080 olarak ayarladık
         int port = 8080;
-
-        if (portEnv != null && !portEnv.isEmpty()) {
-            try {
-                port = Integer.parseInt(portEnv);
-            } catch (NumberFormatException e) {
-                System.err.println("Geçersiz PORT değeri, varsayılan port kullanılacak: " + e.getMessage());
-            }
-        } else {
-            System.out.println("PORT çevresel değişkeni bulunamadı, varsayılan port kullanılacak.");
-        }
 
         System.out.println("Uygulama portu: " + port);
         SpringApplication.run(HztApplication.class, args);
