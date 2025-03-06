0a52f413a873af2ff8e0e5585e95d3c9afcd58f3
novruzmirishli13 <nmirisli13@gmail.com>
2025-03-07 01:10:50 +0400

    eiehhd

--- src/main/java/com/example/HZT/Controller/GameController.java
+++ src/main/java/com/example/HZT/Controller/GameController.java
@@ -1,37 +1,25 @@
 package com.example.HZT.Controller;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.HZT.Service.GameService;
 
 @RestController
 @RequestMapping("/game")
 public class GameController {
-    private final GameService gameService;
 
-    public GameController(GameService gameService) {
-        this.gameService = gameService;
-    }
 
-    @PostMapping("/execute")
-    public ResponseEntity<Object> executeMoves(@RequestBody List<String> commands) {
-       
-        gameService.processCommands(commands);
-
+    @GetMapping("/execute")
+    public ResponseEntity<Object> executeMoves() {
         
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(new ResponseMessage("It's Working Woleyyy"));
     }
 
-   
+
     public static class ResponseMessage {
         private String message;
 
