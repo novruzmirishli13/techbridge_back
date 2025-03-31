f85bfaf2541890486caf18d175556216d62e9746
novruzmirishli13 <nmirisli13@gmail.com>
2025-04-01 01:10:15 +0400

    fremaf23426

--- src/main/java/com/example/HZT/Controller/AuthController.java
+++ src/main/java/com/example/HZT/Controller/AuthController.java
@@ -2,10 +2,6 @@ package com.example.HZT.Controller;
 
 
 import org.apache.coyote.BadRequestException;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,26 +12,24 @@ import com.example.HZT.Model.LoginRequestDto;
 import com.example.HZT.Model.RegisterRequestDto;
 import com.example.HZT.Service.AuthService;
 
-@Controller
-@RequestMapping("/auth")
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
-    private final AuthService authService;
+	private final AuthService authService;
 
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
+	public AuthController(AuthService authService) {
+		this.authService = authService;
+	}
 
-    @GetMapping("/register")
-    public String showRegistrationForm(Model model) {
-        model.addAttribute("registerRequest", new RegisterRequestDto());
-        return "register";
-    }
+	@PostMapping("/register")
+	public String registerUser(@RequestBody RegisterRequestDto registerRequest) throws BadRequestException {
+		return authService.registerUser(registerRequest);
+	}
 
-    @PostMapping("/register")
-    public String registerUser(@ModelAttribute RegisterRequestDto registerRequest, Model model) throws BadRequestException {
-        String result = authService.registerUser(registerRequest);
-        model.addAttribute("message", result);
-        return "register-success";
-    }
+	@PostMapping("/login")
+	public AuthResponse authenticateUser(@RequestBody LoginRequestDto loginRequest) {
+		return authService.loginUser(loginRequest);
+	}
+
 }
